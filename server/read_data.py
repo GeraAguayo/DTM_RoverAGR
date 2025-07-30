d1577373b9f0287eed48943d663a69d735fd1f75
Gerardo Aguayo <geraaguayoc@gmail.com>
2025-07-29 19:22:38 -0600

    Configured to send/receive syslogs

--- server/read_data.py
+++ server/read_data.py
@@ -1,4 +1,4 @@
-import serial 
+import serial
 import RPi.GPIO as GPIO
 import time
 import os
@@ -8,24 +8,39 @@ BAUDRATE = 9600
 ser = serial.Serial("/dev/ttyACM0",9600)
 ser.baudrate = BAUDRATE
 DATA_LIMIT = 3 #Change here acording the number of values the arduino returns
-data = [None,None,None] #0 = temp, 1 = press, 2 = alt 
 
 def get_data():
-	print("Getting data...")
-	serial_counter = 0
+	print("get_data() called")
+	data = [None] * DATA_LIMIT #0 = temp, 1 = press, 2 = alt
+	log_id = None
+
 	while True:
 		read_ser = ser.readline()
-		msg = read_ser.decode()
+		try:
+			msg = read_ser.decode()
+		except:
+			pass
 		if msg == "END\r\n":
-			print("End of packet")
-			serial_counter = 0
-			print(data)
-			return data
-		else:
-			try:
-				#if valid value from sensor received
-				data[serial_counter] = float(read_ser)
-				serial_counter+=1
-			except:
-				#Invalid value from sensor received None
-				pass
+			if log_id is not None:
+				print("Returning log")
+				print("----------")
+				return log_id
+			else:
+				print("Returning sensor data")
+				print("----------")
+				return data
+		elif msg == "DATA\r\n":
+			print("Data from sensor")
+			for i in range(DATA_LIMIT):
+				read_ser = ser.readline()
+				try:
+					#if valid value from sensor received
+					data[i] = float(read_ser)
+				except:
+					#Invalid value from sensor
+					data[i] = 0.0
+		elif msg == "SYSLOG\r\n":
+			print("System log")
+			read_ser = ser.readline()
+			log_id = int(read_ser)
+
--- server/server.py
+++ server/server.py
@@ -18,19 +18,25 @@ while True:
 	client_pair = server_socket.recvfrom(buffer_size)
 	client_ip = client_pair[1]
 	print("Now working with client: ",client_ip)
-	#Start the message
-	server_socket.sendto(str.encode("START_TM"),client_ip)
 
-	#Set the num of values for the client to receive
+	#Get data from DCM
 	data = read_data.get_data()
-	server_socket.sendto(str.encode(str(len(data))),client_ip)
 
-	#Send telemetry values
-	for d in data:
-		val = str.encode(str(d))
-		server_socket.sendto(val,client_ip)
-	data = [0.0,0.0,0.0]
+	#If sensor data received
+	if (type(data) is list):
+		server_socket.sendto(str.encode("START_TM"),client_ip)
+		#send number of values
+		server_socket.sendto(str.encode(str(len(data))),client_ip)
+		#Send telemetry values
+		for d in data:
+			val = str.encode(str(d))
+			server_socket.sendto(val,client_ip)
+		data = [0.0,0.0,0.0]
+	#If system log received
+	elif (type(data) is int):
+		server_socket.sendto(str.encode("SYSLOG"),client_ip)
+		server_socket.sendto(str.encode(str(data)),client_ip)
 
 	#End the message
-	server_socket.sendto(str.encode("END_TM"),client_ip)
+	server_socket.sendto(str.encode("END"),client_ip)
 
